ba7823c061111a4d253867f9711b90297461c4fe
Subhali AR Otti <167698374+subhaliAR21@users.noreply.github.com>
2026-04-24 22:57:10 +0530

    Update api.js

--- influencer-dashboard/src/api.js
+++ influencer-dashboard/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://influencer-affiliate-sales-payment.onrender.com' })
+const api = axios.create({ baseURL: 'https://influencer-affiliate-sales-payment.onrender.com/api' })
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token')
